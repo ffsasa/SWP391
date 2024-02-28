dd6deb9036bc008d4010538da6d7f6bc66d9175b
nguyenhieuhien <nguyenhieuhiendevse@gmail.com>
2024-02-28 13:31:36 +0700

    update

--- booking-birthday-for-kids/src/main/java/com/bookingBirthday/bookingbirthdayforkids/service/PackageServiceService.java
+++ booking-birthday-for-kids/src/main/java/com/bookingBirthday/bookingbirthdayforkids/service/PackageServiceService.java
@@ -1,4 +1,17 @@
 package com.bookingBirthday.bookingbirthdayforkids.service;
 
+import com.bookingBirthday.bookingbirthdayforkids.dto.request.PackageServiceRequest;
+import com.bookingBirthday.bookingbirthdayforkids.dto.response.ResponseObj;
+import org.springframework.http.ResponseEntity;
+
 public interface PackageServiceService {
+    public ResponseEntity<ResponseObj> getAll();
+
+    public ResponseEntity<ResponseObj> getById(Long id);
+
+    public ResponseEntity<ResponseObj> create(PackageServiceRequest packageServiceRequest);
+
+    public ResponseEntity<ResponseObj> update(Long id, PackageServiceRequest packageServiceRequestRequest);
+
+    public ResponseEntity<ResponseObj> delete(Long id);
 }
