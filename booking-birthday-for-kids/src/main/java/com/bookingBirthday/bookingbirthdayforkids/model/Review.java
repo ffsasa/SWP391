6bf2930bb0a8528e9c203bfe641f98ecd1062530
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2024-03-02 23:15:00 +0700

    Update model Account Role Inquiry Review

---
+++ booking-birthday-for-kids/src/main/java/com/bookingBirthday/bookingbirthdayforkids/model/Review.java
@@ -0,0 +1,36 @@
+package com.bookingBirthday.bookingbirthdayforkids.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Review extends BaseEntity{
+    @NotBlank(message = "Message cannot be blank")
+    private String reviewMessage;
+    @NotBlank(message = "Reply message cannot be blank")
+    private String replyMessage;
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating must be at most 5")
+    private float rating;
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    @JsonIgnore
+    private LocalDateTime reviewDate;
+
+
+}
