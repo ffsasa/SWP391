516218b13d59641e03291ba41ec79b03c55664f9
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2024-03-03 14:48:47 +0700

    sync with main branch

---
+++ booking-birthday-for-kids/src/main/java/com/bookingBirthday/bookingbirthdayforkids/controller/AccountController.java
@@ -0,0 +1,7 @@
+package com.bookingBirthday.bookingbirthdayforkids.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AccountController {
+}
---
+++ booking-birthday-for-kids/src/main/java/com/bookingBirthday/bookingbirthdayforkids/dto/request/AccountRequest.java
@@ -0,0 +1,25 @@
+package com.bookingBirthday.bookingbirthdayforkids.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class AccountRequest {
+    @NotBlank(message = "Username can not blank")
+    private String username;
+    @NotBlank(message = "Password can not blank")
+    private String password;
+    @NotBlank(message = "FullName can not blank")
+    private String fullName;
+    @NotBlank(message = "Email can not blank")
+    private String email;
+    @NotBlank(message = "Phone can not blank")
+    private String phone;
+    private LocalDateTime dob;
+}
