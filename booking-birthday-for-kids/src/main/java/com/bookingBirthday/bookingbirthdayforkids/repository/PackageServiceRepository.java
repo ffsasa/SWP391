c6ca69834f0d296e56673fcbead41957de1de8a9
nguyenhieuhien <nguyenhieuhiendevse@gmail.com>
2024-03-06 22:03:11 +0700

    Sync with main branch

---
+++ booking-birthday-for-kids/src/main/java/com/bookingBirthday/bookingbirthdayforkids/repository/PackageServiceRepository.java
@@ -0,0 +1,14 @@
+package com.bookingBirthday.bookingbirthdayforkids.repository;
+
+import com.bookingBirthday.bookingbirthdayforkids.model.Account;
+import com.bookingBirthday.bookingbirthdayforkids.model.Package;
+import com.bookingBirthday.bookingbirthdayforkids.model.PackageService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PackageServiceRepository extends JpaRepository<PackageService, Long> {
+    List<PackageService> findAllByIsActiveIsTrue();
+}
