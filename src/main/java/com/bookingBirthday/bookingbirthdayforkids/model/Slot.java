d2dfef9b0895dc95a748dfb9db8e2d0cd09f9978
nguyenhieuhien <nguyenhieuhiendevse@gmail.com>
2024-03-15 00:44:29 +0700

    Update Validate can not create in 00:00:00 - 07:59:59 and can not create with time start > time end

--- src/main/java/com/bookingBirthday/bookingbirthdayforkids/model/Slot.java
+++ src/main/java/com/bookingBirthday/bookingbirthdayforkids/model/Slot.java
@@ -32,14 +32,4 @@ public class Slot extends BaseEntity {
     @JsonManagedReference
     @JsonIgnore
     private List<SlotInVenue> slotInVenueList;
-
-    public boolean isValidTimeRange() {
-        try {
-            LocalTime start = LocalTime.parse(this.timeStart, DateTimeFormatter.ofPattern("HH:mm:ss"));
-            LocalTime end = LocalTime.parse(this.timeEnd, DateTimeFormatter.ofPattern("HH:mm:ss"));
-            return start.isBefore(end);
-        } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("Invalid time format");
-        }
-    }
     }
