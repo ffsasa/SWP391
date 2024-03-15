e5fcee19920fb829cb3d38258abc4deed3372aee
nguyenhieuhien <nguyenhieuhiendevse@gmail.com>
2024-03-16 02:21:34 +0700

    Update package

--- src/main/java/com/bookingBirthday/bookingbirthdayforkids/controller/PackageController.java
+++ src/main/java/com/bookingBirthday/bookingbirthdayforkids/controller/PackageController.java
@@ -1,5 +1,6 @@
 package com.bookingBirthday.bookingbirthdayforkids.controller;
 
+import com.bookingBirthday.bookingbirthdayforkids.dto.request.PackageServiceRequest;
 import com.bookingBirthday.bookingbirthdayforkids.dto.response.ResponseObj;
 import com.bookingBirthday.bookingbirthdayforkids.service.PackageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/package")
 public class PackageController {
@@ -16,11 +19,12 @@ public class PackageController {
     PackageService packageService;
 
     @GetMapping("/get-all")
-    public ResponseEntity<ResponseObj> getAll(){ return packageService.getAll();
+    public ResponseEntity<ResponseObj> getAll() {
+        return packageService.getAll();
     }
 
     @GetMapping("/get-id/{id}")
-    public ResponseEntity<ResponseObj> getById(@PathVariable Long id){
+    public ResponseEntity<ResponseObj> getById(@PathVariable Long id) {
         return packageService.getById(id);
     }
 
@@ -29,22 +33,22 @@ public class PackageController {
     public ResponseEntity<?> create(@RequestPart(name = "fileImg", required = true) MultipartFile fileImg,
                                     @RequestPart String packageName,
                                     @RequestPart String packageDescription,
-                                    @RequestPart String pricing){
-        return packageService.create(fileImg, packageName, packageDescription, Float.parseFloat(pricing));
+                                    @RequestPart List<PackageServiceRequest> packageServiceRequest) {
+        return packageService.create(fileImg, packageName, packageDescription, packageServiceRequest);
     }
 
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('HOST')")
     @PutMapping(value = "/update-package/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> update( @PathVariable Long id,@RequestPart(name = "fileImg", required = false) MultipartFile fileImg,
-                                     @RequestPart String packageName,
-                                     @RequestPart String packageDescription,
-                                     @RequestPart String pricing){
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestPart(name = "fileImg", required = false) MultipartFile fileImg,
+                                    @RequestPart String packageName,
+                                    @RequestPart String packageDescription,
+                                    @RequestPart String pricing) {
         return packageService.update(id, fileImg, packageName, packageDescription, Float.parseFloat(pricing));
     }
 
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('HOST')")
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<ResponseObj> delete(@PathVariable Long id){
+    public ResponseEntity<ResponseObj> delete(@PathVariable Long id) {
         return packageService.delete(id);
     }
 }
