b7aff516d1558a8036587c595147f729ee11ef53
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2024-03-18 00:25:18 +0700

    Validate pricing create-service, update-service

--- src/main/java/com/bookingBirthday/bookingbirthdayforkids/model/Services.java
+++ src/main/java/com/bookingBirthday/bookingbirthdayforkids/model/Services.java
@@ -1,6 +1,7 @@
 package com.bookingBirthday.bookingbirthdayforkids.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
@@ -36,6 +37,7 @@ public class Services extends BaseEntity{
 
     @OneToMany(mappedBy = "services", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JsonManagedReference
+    @JsonIgnore
     private List<UpgradeService> upgradeServiceList;
 
 }
