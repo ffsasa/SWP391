5ce6c49a0d02d686aeed1a5d127dd38c17272edd
nguyenhieuhien <nguyenhieuhiendevse@gmail.com>
2024-03-27 18:31:07 +0700

    Add new api for send inquiry to change package

--- src/main/java/com/bookingBirthday/bookingbirthdayforkids/service/impl/InquiryQuestionServiceImpl.java
+++ src/main/java/com/bookingBirthday/bookingbirthdayforkids/service/impl/InquiryQuestionServiceImpl.java
@@ -30,7 +30,7 @@ public class InquiryQuestionServiceImpl implements InquiryQuestionService {
     public InquiryQuestionServiceImpl() {
         super();
     }
-    
+
 
     @Override
     public ResponseEntity<ResponseObj> create(InquiryQuestionRequest inquiryRequest) {
@@ -112,4 +112,32 @@ public class InquiryQuestionServiceImpl implements InquiryQuestionService {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseObj(HttpStatus.OK.toString(), "Inquiry", inquiry));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseObj(HttpStatus.BAD_REQUEST.toString(), "Inquiry does not exist", null));
     }
+      @Override
+    public ResponseEntity<ResponseObj> sendInquiryForChangePackageInVenue(Long bookingId, Long packageInVenueId) {
+        Long userId = AuthenUtil.getCurrentUserId();
+        if(userId == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ResponseObj(HttpStatus.UNAUTHORIZED.toString(), "400", null));
+        }
+
+        Optional<PackageInVenue> packageInVenueOptional = packageInVenueRepository.findById(packageInVenueId);
+        String packageName = packageInVenueOptional.get().getApackage().getPackageName();
+
+        Optional<PartyBooking> partyBookingOptional = partyBookingRepository.findById(bookingId);
+        PartyBooking partyBooking = partyBookingOptional.get();
+
+        Account account = accountRepository.findById(userId).get();
+        Inquiry inquiry = new Inquiry();
+        String question = "Tại booking (id = " + bookingId + ") ngày " + partyBooking.getPartyDated().getDate() + " khung giờ từ " + partyBooking.getPartyDated().getSlotInRoom().getSlot().getTimeStart()
+                + " đến " + partyBooking.getPartyDated().getSlotInRoom().getSlot().getTimeEnd()
+                + ". Tôi muốn thay đổi gói dịch vụ bữa tiệc thành " + packageName.toUpperCase();
+        inquiry.setInquiryQuestion(question);
+        inquiry.setActive(true);
+        inquiry.setStatus(InquiryStatus.PENDING);
+        inquiry.setCreateAt(LocalDateTime.now());
+        inquiry.setUpdateAt(LocalDateTime.now());
+        inquiry.setAccount(account);
+        inquiryRepository.save(inquiry);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseObj(HttpStatus.CREATED.toString(), "Create Inquiry Successful", question));
+    }
+
 }
