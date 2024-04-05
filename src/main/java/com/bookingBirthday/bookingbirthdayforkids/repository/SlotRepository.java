d266fbf337a776260bc1357aa5ae19fffd23c8cc
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2024-04-05 15:37:29 +0700

    Paging partyBooking for host

--- src/main/java/com/bookingBirthday/bookingbirthdayforkids/repository/SlotRepository.java
+++ src/main/java/com/bookingBirthday/bookingbirthdayforkids/repository/SlotRepository.java
@@ -2,6 +2,7 @@ package com.bookingBirthday.bookingbirthdayforkids.repository;
 
 import com.bookingBirthday.bookingbirthdayforkids.model.Account;
 import com.bookingBirthday.bookingbirthdayforkids.model.Slot;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
